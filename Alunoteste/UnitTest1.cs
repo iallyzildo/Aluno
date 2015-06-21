3bfa0e106831c1f53e14cd5b745a3d33ed3a4bac
ially <ially1994@hotmail.com>
2015-06-21 18:57:12 -0300

    test

---
+++ Alunoteste/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alunoteste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 0;
+
+            Assert.IsTrue(x==1);
+        }
+    }
+}
