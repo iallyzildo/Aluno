58fe2af66aa5784335a88256319d7840204f1fb6
ially <ially1994@hotmail.com>
2015-06-28 13:15:14 -0300

    testando mais

--- Aluno.Tests/AlunoModelTests.cs
+++ Aluno.Tests/AlunoModelTests.cs
@@ -2,20 +2,58 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
 using Aluno.Models;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+using Aluno.Entity;
+using Aluno.Controllers;
+
 namespace Aluno.Tests
 {
     [TestClass]
     public class AlunoModelTests
     {
-        [TestMethod]
-        public void AlunoCPFTest()
+
+        private AlunoModel alunoModel;
+
+        [TestInitialize]
+        public void IniciarTestes()
         {
-            AlunoModel target = new AlunoModel();
+            alunoModel = new AlunoModel();
+        }
+
+        [TestCleanup]
+        public void FinalizarTestes()
+        {
+            Debug.WriteLine("Teste finalizado");
+        }
+
+
+
+
+        [TestMethod]
+        public void TestarDigitosVerificadoresCpf()
+        {          
             string digitos = "112957216";
             string experado = "19";
             string validacao;
-            validacao = target.calcularDigitosVerificadoresCPF(digitos);
+            validacao = alunoModel.calcularDigitosVerificadoresCPF(digitos);
             Assert.AreEqual(experado, validacao);
         }
+        [TestMethod]
+        public void PegarAluno1_DeverRetornarAluno1()
+        {
+            var result = alunoModel.obterAluno(1);
+            Assert.AreEqual(alunoModel.obterAluno(1), result);
+        }
+        [TestMethod]
+        public void PegarAluno_RetornarNomeCorreto()
+        {
+            var result = alunoModel.obterAluno(1);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(alunoModel.obterAluno(1).Nome, result.Nome);
+        }
+           
     }
 }
