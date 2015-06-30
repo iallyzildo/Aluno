a084ef9659e3778ea11b669c6f7fd000cb8ad171
Ially Leandro <ially1994@hotmail.com>
2015-06-30 11:05:40 -0300

    Arrumando os Testes

--- TelasSelenium.Test/TelasTest.cs
+++ TelasSelenium.Test/TelasTest.cs
@@ -25,7 +25,7 @@ namespace TestFrontEnd
         }
 
         [TestMethod]
-        public void Excluir_Aluno()
+        public void Excluir_AlunoCancelando()
         {
             driver.FindElement(By.CssSelector("a[href*='/Aluno']")).Click();
             IWebElement Delete = driver.FindElement(By.LinkText("Delete"));
@@ -47,28 +47,6 @@ namespace TestFrontEnd
             IWebElement searchInput = driver.FindElement(By.Id("a"));
             searchInput.SendKeys("ferreira");
             searchInput.SendKeys(Keys.Enter);
-        }
-
-        [TestMethod]
-        public void AdicionarAluno()
-        {
-            //Entra na pagina de aluno
-            driver.FindElement(By.CssSelector("a[href*='/Aluno']")).Click();
-            //Clica no botao novo
-            driver.FindElement(By.XPath("//*[@id='Novo']")).Click();
-            //preenche o campo nome
-            IWebElement Nome = driver.FindElement(By.Id("Nome"));
-            Nome.SendKeys("iallyzildo");
-            //preenche o campo CPF
-            IWebElement CPF = driver.FindElement(By.Id("CPF"));
-            CPF.SendKeys("12281028640");
-            //preenche o campo nome
-            IWebElement Matricula = driver.FindElement(By.Id("Matricula"));
-            Matricula.SendKeys("87654321");
-            Matricula.SendKeys(Keys.Enter);
-
-            string lista = driver.FindElement(By.Id("Lista")).Text;
-            Assert.IsTrue(lista.Contains("iallyzildo"));
         }
 
     }
